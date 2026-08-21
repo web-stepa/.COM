51a5392f7bd297f4c55bbb873d39601216befc7c
web-stepa <julyancaesar0207@gmail.com>
2026-08-21 13:24:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* ================= KONFIGURASI ================= */
 
 // URL Apps Script Web App Terbaru
-const API_URL = "https://script.google.com/macros/s/AKfycbxBa2DZVgsvAr_gMrJ6JeJK6t54_FaFKNzRy6e7YM2ese1VXow6t1xVF27E1-2yWzUqRw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxBAHyviSvcV5jIjA4s3kCWk85hwxlYcoDd6Mlo5WjkVBlcgR2MGz5wItAilVDDsiCc6w/exec";
 
 // Global Data
 let currentUser = null;
